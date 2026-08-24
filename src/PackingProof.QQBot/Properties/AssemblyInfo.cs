8dfddb264009c93164a5c88fa27461ef243a6c22
m-RNA <m-RNA@qq.com>
2026-08-25 00:56:14 +0800

    fix: 标注 QQBot 的 Windows 运行平台
    保留跨平台目标框架的轻量发布方式，同时明确 Windows 加密存储的运行平台要求。

--- src/PackingProof.QQBot/Properties/AssemblyInfo.cs
+++ src/PackingProof.QQBot/Properties/AssemblyInfo.cs
@@ -1,3 +1,5 @@
 using System.Runtime.CompilerServices;
+using System.Runtime.Versioning;
 
 [assembly: InternalsVisibleTo("PackingProof.QQBot.Tests")]
+[assembly: SupportedOSPlatform("windows10.0.19041.0")]
